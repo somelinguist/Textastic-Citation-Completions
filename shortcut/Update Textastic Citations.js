a4276286fc631af9a18dbff38a68579e66d7b969
Kevin Cline <admin@somelinguist.org>
2019-12-23 15:51:58 -0600

    support completions in (La)TeX cite commands
    Adds support for completions citation commands in (La)TeX, including
    BibTeX, BibLaTeX, and natbib

--- shortcut/Update Textastic Citations.js
+++ shortcut/Update Textastic Citations.js
@@ -27,23 +27,140 @@ let completionFile = {
 	uuid: "6a6b74bb-499d-43cf-89f7-8a2d0815d0d8",
 	completionSets: [	],
 	contexts: [
-		{
-			description: "Bibliographic reference completions",
-			scope: "text.pandoc, text.html.markdown",
-			pattern: "@(.*)",
-			completionCaptureIndex: 1,
-			completionSetNames: [
-				"pandoc.references"
-			]
-		}
-	]
+      {
+        description: "Pandoc/markdown reference completions",
+        scope: "text.pandoc, text.html.markdown",
+        pattern: "@(.*)",
+        completionCaptureIndex: 1,
+        completionSetNames: [
+          "citation.references"
+        ]
+      },
+      {
+        description: "Bib(La)TeX reference completions - cite",
+        scope: "text.tex.latex",
+        pattern: "\\\\[cC]ite(\\[.*?\\]){0,2}\\{(?:[^\\}]*,)*([^\\}]*)",
+        completionCaptureIndex: 2,
+        completionSetNames: [
+          "citation.references"
+        ]
+      },
+      {
+        description: "BibLaTeX reference completions - cites",
+        scope: "text.tex.latex",
+        pattern: "\\\\[cC]ites(?:(\\(.*?\\)){0,2}(\\[.*?\\]){0,2}\\{.*?\\})*(\\[.*?\\]){0,2}\\{([^\\}]*)",
+        completionCaptureIndex: 4,
+        completionSetNames: [
+          "citation.references"
+        ]
+      },
+      {
+        description: "BibLaTeX reference completions 2",
+        scope: "text.tex.latex",
+        pattern: "\\\\(foot|[pP]aren|[tT]ext|[sS]mart|super|[aA]uto)cite(\\[.*?\\]){0,2}\\{([^\\}]*)",
+        completionCaptureIndex: 3,
+        completionSetNames: [
+          "citation.references"
+        ]
+      },
+      {
+        description: "BibLaTeX reference completions 2 multi",
+        scope: "text.tex.latex",
+        pattern: "\\\\(foot|[pP]aren|[tT]ext|[sS]mart|super|[aA]uto)cites(?:(\\(.*?\\)){0,2}(\\[.*?\\]){0,2}\\{.*?\\})*(\\[.*?\\]){0,2}\\{([^\\}]*)",
+        completionCaptureIndex: 5,
+        completionSetNames: [
+          "citation.references"
+        ]
+      },
+      {
+        description: "BibLaTeX reference completions 3",
+        scope: "text.tex.latex",
+        pattern: "\\\\cite(author|title|year|date|url)(\\[.*?\\]){0,2}\\{([^\\}]*)",
+        completionCaptureIndex: 3,
+        completionSetNames: [
+          "citation.references"
+        ]
+      },
+      {
+        description: "BibLaTeX reference completions 4 *",
+        scope: "text.tex.latex",
+        pattern: "\\\\(cite|citeauthor|citetitle|parencite|autocite)\\*(\\[.*?\\]){0,2}\\{([^\\}]*)",
+        compltionCaptureIndex: 3,
+        completionSetNames: [
+          "citation.references"
+        ]
+      },
+      {
+        description: "BibLaTeX reference completions 5",
+        scope: "text.tex.latex",
+        pattern: "\\\\(full|footfull|[nN]ote|[pP]note|fnote)cite(\\[.*?\\]){0,2}\\{([^\\}]*)",
+        completionCaptureIndex: 3,
+        completionSetNames: [
+          "citation.references"
+        ]
+      },
+      {
+        description: "BibLaTeX reference completions 6",
+        scope: "text.tex.latex",
+        pattern: "\\\\([vV]|[pP]v|[fF]v|[sS]v|[tT]v|[aA]v|ftv)olcite(\\[.*?\\])?\\{.*?\\}(\\[.*?\\])?\\{([^\\}]*)",
+        completionCaptureIndex: 4,
+        completionSetNames: [
+          "citation.references"
+        ]
+      },
+      {
+        description: "BibLaTeX reference completions 6 multi",
+        scope: "text.tex.latex",
+        pattern: "\\\\([vV]|[pP]v|[fF]v|[sS]v|[tT]v|[aA]v|ftv)olcites(?:(\\(.*?\\)){0,2}(\\[.*?\\])?\\{.*?\\}(\\[.*?\\])?\\{.*?\\})*(\\[.*?\\])?\\{.*?\\}?(\\[.*?\\])?\\{([^\\}])*",
+        completionCaptureIndex: 7,
+        completionSetNames: [
+          "citation.references"
+        ]
+      },
+      {
+        description: "BibLaTeX reference completions - foottextcite",
+        scope: "text.tex.latex",
+        pattern: "\\\\foottextcite(\\[.*?\\]){0,2}\\{([^\\}]*)",
+        completionCaptureIndex: 2,
+        completionSetNames: [
+          "citation.references"
+        ]
+      },
+      {
+        description: "BibLaTeX reference completions - foottextcites",
+        scope: "text.tex.latex",
+        pattern: "\\\\foottextcites(?:(\\(.*?\\)){0,2}(\\[.*?\\]){0,2}\\{.*?\\})*(\\[.*?\\]){0,2}\\{([^\\}]*)",
+        completionCaptureIndex: 4,
+        completionSetNames: [
+          "citation.references"
+        ]
+      },
+      {
+        description: "natbib reference completions",
+        scope: "text.tex.latex",
+        pattern: "\\\\[cC]ite(al)?(t|p)(\\[.*?\\]){0,2}\\{(?:[^\\}]*,)*([^\\}]*)",
+        completionCaptureIndex: 4,
+        completionSetNames: [
+          "citation.references"
+        ]
+      },
+      {
+        description: "natbib reference completions - 2",
+        scope: "text.tex.latex",
+        pattern: "\\\\cite(num|yearpar)(\\[.*?\\]){0,2}\\{(?:[^\\}]*,)*([^\\}]*)",
+        completionCaptureIndex: 3,
+        completionSetNames: [
+          "citation.references"
+        ]
+      }
+    ]
 };
 
 let references = {
-			name: "pandoc.references",
-			defaultAppend: "",
-			strings: []
-		}
+	name: "citation.references",
+	defaultAppend: "",
+	strings: []
+}
 
 for (match of bib.matchAll(/@([a-zA-Z]*)\s*\{\s*([a-zA-Z0-9_:.#$%&-+?<>~/]*),.*?^\s*title\s*=\s*[\{"](.*?)[\\}"],$/gmis)) {
   let t = match[1].toLowerCase();
@@ -51,6 +168,7 @@ for (match of bib.matchAll(/@([a-zA-Z]*)\s*\{\s*([a-zA-Z0-9_:.#$%&-+?<>~/]*),.*?
     references.strings.push({"string": match[2]+" | "+match[3].replace(/[\{\}]/g, ""), "replace": match[2]});
   }
 }
+
 
 completionFile.completionSets.push(references);
 
